d69d6171e4c2310d396528afd776df661636db37
unknown <hdmcgehee@gmail.com>
2020-09-03 20:43:17 -0400

    git init

---
+++ AudioClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+[System.Serializable]
+
+public class AudioClass
+{
+    public string name;
+
+    public AudioClip[] Clip;
+
+    public bool loop;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(-2f, 2f)]
+    public float pitch;
+
+    [Range(0, 256)]
+    public int priority;
+
+    public AudioMixerGroup output;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
+
---
+++ PlayAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayAudio : MonoBehaviour
+{
+
+    public AudioClass[] sounds;
+
+    float minPitch = .95f;
+    float maxPitch = 1.05f;
+
+    public void AudioNewPlay(string name)
+    {
+        AudioClass s = System.Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        int clipIndex = Random.Range(0, s.Clip.Length);
+
+        s.source = gameObject.AddComponent<AudioSource>();
+        s.source.clip = s.Clip[clipIndex];
+        s.source.volume = s.volume;
+        s.pitch = Random.Range(minPitch, maxPitch);
+        s.source.priority = s.priority;
+        s.source.loop = s.loop;
+        s.source.outputAudioMixerGroup = s.output;
+
+        s.source.Play();
+
+        Destroy(s.source, s.Clip[clipIndex].length);
+    }
+
+}
